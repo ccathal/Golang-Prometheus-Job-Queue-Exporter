92ebaba7d721ba344dc9bebf2b50a697be253ed7
ccathal <cathalcorbett3@gmail.com>
2020-08-06 12:48:34 +0100

    Folder structure refactor

---
+++ tests/invalid_data_test.go
@@ -0,0 +1,28 @@
+package exporter_tests
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"github.com/stretchr/testify/assert"
+	"exporter/parser"
+)
+
+func TestInvalidData(t *testing.T) {
+	file, err := os.Open("test_data/slurm-out-invalid.txt")
+        if err != nil {
+		t.Fatalf("Can not open test data: %v", err)
+        }
+        data, err := ioutil.ReadAll(file)
+
+	mp := parser.ParseQueueMetrics(data)
+
+	counter := 0
+	for k := range mp {
+		squeue_jobs := mp[k]
+		for i := range squeue_jobs {
+			counter += int(squeue_jobs[i])
+		}
+	}
+	assert.Equal(t, counter, int(25))
+}
